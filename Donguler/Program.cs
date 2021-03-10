35a8b666fe166be7deb4f9ca4e43cb6a27b73f63
ACER <ACER@DESKTOP-TRKNRSV>
2021-03-10 11:54:19 +0300

    Add project files.

---
+++ Donguler/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //array-dizi 
+            string[] kurslar = new string[] { "Yazılım Geliştirme Kampı", "programlamaya Başlangıç için Temel Kurs", "Java" };
+
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+
+            }
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+
+            }
+            Console.WriteLine("Sayfa Sonu-footer");
+        }
+    }
+}
